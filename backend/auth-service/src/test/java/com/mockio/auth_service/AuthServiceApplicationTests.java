4095d0d684c57c588a8ff54b8470d3d167d599bb
cohouseol <cohouseol@gmail.com>
2025-11-29 16:31:21 +0900

    init : MSA 구조 이용하여 프로젝트 INIT

---
+++ backend/auth-service/src/test/java/com/mockio/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockio.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

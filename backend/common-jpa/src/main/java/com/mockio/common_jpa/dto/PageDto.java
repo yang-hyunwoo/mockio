efe073c25ad21d519146692783532bef309d2619
cohouseol <cohouseol@gmail.com>
2025-12-04 23:18:59 +0900

    Feat : 유저프로필 등록 작업 진행중
    - keycloak 회원가입 후 로그인 시 유저 프로필 등록되게 작업 진행중

---
+++ backend/common-jpa/src/main/java/com/mockio/common_jpa/dto/PageDto.java
@@ -0,0 +1,42 @@
+package com.mockio.common_jpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageDto<T> {
+
+    private List<T> content;          // 데이터 리스트
+
+    private int pageNumber;           // 현재 페이지
+
+    private int pageSize;             // 페이지 크기
+
+    private long totalElements;       // 전체 데이터 개수
+
+    private int totalPages;           // 전체 페이지 수
+
+
+    public static <E, D> PageDto<D> of(Page<E> page, Function<E, D> mapper) {
+        List<D> dtoList = page.getContent()
+                .stream()
+                .map(mapper)
+                .toList();  // Java 17 이상
+
+        return new PageDto<>(
+                dtoList,
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages()
+        );
+    }
+
+}
---
+++ backend/user-service/src/main/java/com/mockio/user_service/dto/UserProfileDto.java
@@ -0,0 +1,33 @@
+package com.mockio.user_service.dto;
+
+import com.mockio.user_service.domain.UserProfile;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record UserProfileDto(
+        @Schema(description = "유저_pk", example = "1")
+        Long id,
+        @Schema(description = "keycloak_Pk", example = "1")
+        String keycloakId,
+        @Schema(description = "유저_프로필_이미지_pk", example = "1")
+        Long profileImageId,
+        @Schema(description = "유저_이름", example = "홍길동")
+        String name,
+        @Schema(description = "유저_이메일", example = "hong@example.com")
+        String email,
+        @Schema(description = "유저_닉네임", example = "gusdn")
+        String nickname,
+        @Schema(description = "유저_휴대폰번호", example = "01012345678")
+        String phoneNumber
+) {
+    public static UserProfileDto from(UserProfile userProfile) {
+        return new UserProfileDto(
+                userProfile.getId(),
+                userProfile.getKeycloakId(),
+                userProfile.getProfileImageId(),
+                userProfile.getName(),
+                userProfile.getEmail(),
+                userProfile.getNickname(),
+                userProfile.getPhoneNumber()
+        );
+    }
+}

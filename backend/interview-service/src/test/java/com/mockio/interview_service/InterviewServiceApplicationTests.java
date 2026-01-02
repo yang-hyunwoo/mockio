309e67d9f103bbaf4318924268393dd296bd2f04
cohouseol <cohouseol@gmail.com>
2026-01-02 15:44:49 +0900

    Feat : msa 서비스 init
    - ai-serivce
    - notification-service
    - interview-service
    - chat-service
    
    추가 init

---
+++ backend/interview-service/src/test/java/com/mockio/interview_service/InterviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockio.interview_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

06a88ad23ca3c98738a12550337eef21a4dad605
cohouseol <cohouseol@gmail.com>
2026-02-07 17:40:42 +0900

    Feat : 공지사항/1:1문의/Faq 서비스 init

---
+++ backend/inquiry-service/src/main/java/com/mockio/inquiry_service/InquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mockio.inquiry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/noti-service/src/test/java/com/mockio/noti_service/NotiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockio.noti_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

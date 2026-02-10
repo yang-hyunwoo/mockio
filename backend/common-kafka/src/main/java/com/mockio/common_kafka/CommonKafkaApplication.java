d29e8bbb49b6885bf318a159115feb884d632a6d
cohouseol <cohouseol@gmail.com>
2026-02-10 23:02:40 +0900

    refactor : kafka 공통서비스로 변환 작업
    - TODO : outbox 는 있지만 inbox가 없어서 inbox 작업하기

---
+++ backend/common-kafka/src/main/java/com/mockio/common_kafka/CommonKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mockio.common_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonKafkaApplication.class, args);
+	}
+
+}
---
+++ backend/common-kafka/src/main/java/com/mockio/common_kafka/config/KafkaConsumerConfig.java
@@ -0,0 +1,27 @@
+package com.mockio.common_kafka.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+
+@AutoConfiguration
+public class KafkaConsumerConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            DefaultErrorHandler kafkaErrorHandler
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(kafkaErrorHandler);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        return factory;
+    }
+}

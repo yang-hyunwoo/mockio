bedf63f5faf004ea00a0e64bf61514447978afea
cohouseol <cohouseol@gmail.com>
2026-03-16 21:44:40 +0900

    [feat] : faq 서비스 로직 개발

--- backend/faq-service/src/main/java/com/mockio/faq_service/controller/FaqController.java
+++ backend/faq-service/src/main/java/com/mockio/faq_service/controller/FaqController.java
@@ -1,6 +1,5 @@
 package com.mockio.faq_service.controller;
 
-import com.mockio.common_jpa.dto.PageDto;
 import com.mockio.common_spring.util.MessageUtil;
 import com.mockio.common_spring.util.response.Response;
 import com.mockio.faq_service.dto.request.FaqReqDto;
@@ -10,8 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,13 +26,12 @@ public class FaqController {
     private final MessageUtil messageUtil;
 
     @Operation(summary = "Faq 리스트 조회", description = "Faq 리스트 조회")
-    @GetMapping("/faq")
-    public ResponseEntity<Response<PageDto<FaqResDto>>> faqList(
-            @PageableDefault(size = 10, page = 0) Pageable pageable,
+    @GetMapping("/faq/list")
+    public ResponseEntity<Response<FaqResDto>> faqList(
             FaqReqDto faqReqDto
     ) {
         return Response.ok(messageUtil.getMessage("response.read"),
-                faqService.findFaqListPage(pageable, faqReqDto));
+                faqService.findFaqList(faqReqDto));
     }
 
 }
--- backend/faq-service/src/main/java/com/mockio/faq_service/dto/response/FaqResDto.java
+++ backend/faq-service/src/main/java/com/mockio/faq_service/dto/response/FaqResDto.java
@@ -1,24 +1,24 @@
 package com.mockio.faq_service.dto.response;
 
+import com.mockio.common_spring.util.response.EnumResponse;
 import com.mockio.faq_service.constant.FaqType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
-@Builder
-public class FaqResDto {
+import java.util.List;
+import java.util.Set;
 
-    @Schema(description = "FaqPk",example = "1")
-    private Long id;
-
-    @Schema(description = "질문" ,example = "출석은 어떻게")
-    private String question;
-
-    @Schema(description = "답변",example = "출석은 이렇게 하면 되요")
-    private String answer;
-
-    @Schema(description = "FAQ 유형", example = "ALL")
-    private FaqType faqType;
-
+public record FaqResDto(
+        List<Item> items
+) {
+    public record Item(
+            int id,
+            @Schema(description = "질문" ,example = "출석은 어떻게")
+            String question,
+            @Schema(description = "답변",example = "출석은 이렇게 하면 되요")
+            String answer,
+            @Schema(description = "FAQ 유형", example = "ALL")
+            EnumResponse faqType
+    ) {}
 }

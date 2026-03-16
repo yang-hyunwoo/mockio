c0492ff6ee7f4d23eade8ca7053c146168c8d86c
cohouseol <cohouseol@gmail.com>
2026-03-16 21:44:40 +0900

    [feat] : faq 서비스 로직 개발

--- backend/faq-service/src/main/java/com/mockio/faq_service/service/FaqService.java
+++ backend/faq-service/src/main/java/com/mockio/faq_service/service/FaqService.java
@@ -1,14 +1,13 @@
 package com.mockio.faq_service.service;
 
-import com.mockio.common_jpa.dto.PageDto;
 import com.mockio.faq_service.Mapper.FaqMapper;
 import com.mockio.faq_service.dto.request.FaqReqDto;
 import com.mockio.faq_service.dto.response.FaqResDto;
 import com.mockio.faq_service.repository.FaqQueryDslRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -18,16 +17,14 @@ public class FaqService {
     private final FaqQueryDslRepository faqQueryDslRepository;
 
     /**
-     * FAQ 페이징 조회
-     * @param pageable
+     * FAQ  조회
      * @param faqReqDto
      * @return faq 페이지 객체
      */
     @Transactional(readOnly = true)
-    public PageDto<FaqResDto> findFaqListPage(Pageable pageable,
-                                              FaqReqDto faqReqDto
-    ) {
-        return PageDto.of(faqQueryDslRepository.findFaqListPage(pageable, faqReqDto), FaqMapper::from);
+    public FaqResDto findFaqList(FaqReqDto faqReqDto) {
+
+        return FaqMapper.fromList(faqQueryDslRepository.findFaqList(faqReqDto));
     }
 
 }
